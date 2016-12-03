2697772bf231ceb6cd8ec9751a9deb27300685d4
James Bradley <jwbradley87@gmail.com>
2016-12-03 17:36:22 +0000

    Installation layout updates

--- app/components/Installation/component.js
+++ app/components/Installation/component.js
@@ -19,20 +19,24 @@ const Installation = ({ userProfile }) => {
         <div className={styles.panel}>
           <h2>HTML Code</h2>
           <p>Embded this code snippet directly before the closing <code>{"</body>"}</code> tag.</p>
-          <code>{`<script
-                    type=\"text/javascript\"
-                    src=\"https://widget.asktina.io/latest.js\"
-                    data-id=\"${userProfile.user_metadata.slug}\"></script>`}
-          </code>
+          <p>
+            <code>{`<script
+                      type=\"text/javascript\"
+                      src=\"https://widget.asktina.io/latest.js\"
+                      data-id=\"${userProfile.user_metadata.slug}\"></script>`}
+            </code>
+          </p>
         </div>
         <div className={styles.panel}>
           <h2>Wordpress Plugin</h2>
           <p>Download the Wordpress plugin from the link below, or search for "AskTina" directly in the plugins screen in wp-admin.</p>
-          <a
-            href="https://wordpress.org/plugins/asktina-widget/installation/"
-            target="_new">
-            https://wordpress.org/plugins/asktina-widget/installation/
-          </a>
+          <p>
+            <a
+              href="https://wordpress.org/plugins/asktina-widget/installation/"
+              target="_new">
+              https://wordpress.org/plugins/asktina-widget/installation/
+            </a>            
+          </p>
         </div>
         <div className={styles.panel}>
           <h2>Squarespace</h2>
@@ -43,11 +47,13 @@ const Installation = ({ userProfile }) => {
               these instructions
             </a> for code injection to add the following snippet to your Squarespace website:
           </p>
-          <code>{`<script
-              type=\"text/javascript\"
-              src=\"https://widget.asktina.io/latest.js\"
-              data-id=\"${userProfile.user_metadata.slug}\"></script>`}
-          </code>
+          <p>
+            <code>{`<script
+                type=\"text/javascript\"
+                src=\"https://widget.asktina.io/latest.js\"
+                data-id=\"${userProfile.user_metadata.slug}\"></script>`}
+            </code>
+          </p>
         </div>
       </div>
     </div>
